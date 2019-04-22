9a30584ca8959ed1dc8864ea74cfed8296eb7d59
Karthick Paramasivam <37118489+Karthi20@users.noreply.github.com>
2019-04-22 21:00:46 +0530

    Delete config.js

--- TwitterAPI/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
- consumer_key:         'q5fu1TvUYHv71BBuFGn2G050h',
-  consumer_secret:      'Z06dW57PYnohgX9Rw3XwbkMq39bkoc7tqRrbkp5OTFhBuzsuX9',
-  access_token:         '777060436533317632-hYH7KWGAzqS1NxxYU6TBDk90FA44zfw',
-  access_token_secret:  'WOAzyrhA6glErG5BCK4wr6yncYOJ17tBidXTwANQW6dMT'
-}
